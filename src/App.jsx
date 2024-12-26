b78b13f6f99764d434fbf6adb4474f6a2db00bbd
MohamedHamdy210 <mohamed.hamdyy210@gmail.com>
2024-12-26 14:08:34 +0200

    title

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-
 import { useState, useEffect } from "react";
-import Cover from "./cover";
-import List from "./List";
-import Page from "./Page";
-import { createBrowserRouter,RouterProvider } from "react-router-dom";
-import Model from "./Model";
+import Cover from "./components/Cover";
+import List from "./components/List";
+import Page from "./components/Page";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Model from "./components/Model";
 
 function App() {
   const [popular, setPopular] = useState([]);
@@ -14,27 +13,38 @@ function App() {
   const [isModelVisiable, setIsModelVisiable] = useState(false);
   const [model, setModel] = useState({});
   const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: import.meta.env.VITE_APIK,
-  }}
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: import.meta.env.VITE_APIK,
+    },
+  };
 
-  useEffect(()=>{
-    fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
-  .then(res => res.json())
-  .then(res => setTopRated(res.results))
-  .catch(err => console.error(err));
-    fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
-  .then(res => res.json())
-  .then(res => setPopular(res.results))
-    fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
-  .then(res => res.json())
-  .then(res =>{ setNowPlaying(res.results)
-    setLoading(false)
-  })
-  .catch(err => console.error(err));
-  },[])
+  useEffect(() => {
+    fetch(
+      "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
+      options
+    )
+      .then((res) => res.json())
+      .then((res) => setTopRated(res.results))
+      .catch((err) => console.error(err));
+    fetch(
+      "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
+      options
+    )
+      .then((res) => res.json())
+      .then((res) => setPopular(res.results));
+    fetch(
+      "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",
+      options
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        setNowPlaying(res.results);
+        setLoading(false);
+      })
+      .catch((err) => console.error(err));
+  }, []);
 
   const handleClick = (movie) => {
     setModel(movie);
@@ -45,19 +55,23 @@ function App() {
       path: "/",
       element: (
         <>
-        <Cover popular={popular} handleClick={handleClick} />
-        {isModelVisiable && (
-          <Model model={model} setIsModelVisiable={setIsModelVisiable} />
-        )}
-        <div className="lists">
-          <List handleClick={handleClick} name="Popular" arr={[...popular]} />
-          <List
-            handleClick={handleClick}
-            name="Now Playing"
-            arr={[...nowPlaying]}
-          />
-          <List handleClick={handleClick} name="Top Rated" arr={[...topRated]} />
-        </div>
+          <Cover popular={popular} handleClick={handleClick} />
+          {isModelVisiable && (
+            <Model model={model} setIsModelVisiable={setIsModelVisiable} />
+          )}
+          <div className="lists">
+            <List handleClick={handleClick} name="Popular" arr={[...popular]} />
+            <List
+              handleClick={handleClick}
+              name="Now Playing"
+              arr={[...nowPlaying]}
+            />
+            <List
+              handleClick={handleClick}
+              name="Top Rated"
+              arr={[...topRated]}
+            />
+          </div>
         </>
       ),
     },
@@ -106,7 +120,6 @@ function App() {
     },
   ]);
 
-
-  return  loading?<h1>Loading</h1>:<RouterProvider router={router} />;
+  return loading ? <h1>Loading</h1> : <RouterProvider router={router} />;
 }
 export default App;
